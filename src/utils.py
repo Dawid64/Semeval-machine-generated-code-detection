6d59ec28c17713a610f110ae398bd0e3a654cbc5
Dawid64 <dawid.siera@gmail.com>
2025-11-12 23:21:33 +0100

    Add load_data function and torch dependencies

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import shutil
+from typing import Literal
 import kagglehub
 from pathlib import Path
+
+import pandas as pd
 
 
 def download():
@@ -13,6 +16,31 @@ def download():
     data.mkdir(exist_ok=True)
     shutil.move(Path(path, "SemEval-2026-Task13"), data)
     print(f"Moved files to {data}")
+
+
+def load_data(
+    task: Literal["a", "b", "c"], data_part: Literal["train", "val", "test"]
+) -> pd.DataFrame:
+    if Path("data").exists():
+        path = Path("data", "SemEval-2026-Task13")
+    elif Path("..", "data").exists():
+        path = Path("..", "data", "SemEval-2026-Task13")
+    else:
+        raise FileNotFoundError("Dataset not found")
+    dir_paths = {name: path / f"task_{name}" for name in ["a", "b", "c"]}
+    datasets = {
+        name: {
+            "train": dir_paths[name] / f"task_{name}_training_set.parquet"
+            if name == "b"
+            else dir_paths[name] / f"task_{name}_training_set_1.parquet",
+            "val": dir_paths[name] / f"task_{name}_validation_set.parquet",
+            "test": dir_paths[name] / f"task_{name}_test_set_sample.parquet",
+        }
+        for name in ["a", "b", "c"]
+    }
+    data = pd.read_parquet(datasets[task][data_part])
+    data.drop(["generator"], axis=1, inplace=True)
+    return data
 
 
 if __name__ == "__main__":
