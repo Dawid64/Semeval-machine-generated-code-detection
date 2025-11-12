407723c4ff0c0b81d95d1576a645d87bda0d94bb
Dawid64 <dawid.siera@gmail.com>
2025-11-12 11:08:52 +0100

    Updated download script

--- src/utils.py
+++ src/utils.py
@@ -4,16 +4,15 @@ from pathlib import Path
 
 
 def download():
-    #
-    if Path("SemEval-2026-Task13").exists():
+    if Path("data", "SemEval-2026-Task13").exists():
         return
     path = kagglehub.dataset_download("daniilor/semeval-2026-task13")
-
     print("Path to dataset files:", path)
 
-    path = f"{Path.home()}/.cache/kagglehub/datasets/daniilor/semeval-2026-task13/versions/3"
-
-    shutil.move(Path(path, "SemEval-2026-Task13"), Path.cwd() / "data")
+    data = Path("data")
+    data.mkdir(exist_ok=True)
+    shutil.move(Path(path, "SemEval-2026-Task13"), data)
+    print(f"Moved files to {data}")
 
 
 if __name__ == "__main__":
