6bb0acb4678e040c55babac4eaf2c661483aa3de
Dawid64 <dawid.siera@gmail.com>
2025-11-15 23:35:46 +0100

    Add adapter from tree-sitter to torch-geometric

--- src/code_parser.py
+++ src/code_parser.py
@@ -1,8 +1,10 @@
-from pathlib import Path
 from typing import Literal
 
 import pandas as pd
 import networkx as nx
+
+import torch
+from torch_geometric.data import Data
 
 from tree_sitter import Parser, Language, Node, Tree
 import tree_sitter_cpp
@@ -30,7 +32,8 @@ LANGUAGE_MAP: dict[SUPPORTED_LANGUAGES, Language | None] = {
 }
 
 
-def _tree_to_graph(tree: Tree, code_str: str) -> nx.DiGraph:
+def create_nx_graph(code: str, language: SUPPORTED_LANGUAGES) -> nx.DiGraph:
+    tree = _parse_code(code, language)
     root_node = tree.root_node
     g = nx.DiGraph()
     stack: list[tuple[Node, None | int]] = [(root_node, None)]
@@ -41,7 +44,7 @@ def _tree_to_graph(tree: Tree, code_str: str) -> nx.DiGraph:
         this_id = node_id
         node_id += 1
 
-        snippet = code_str[node.start_byte : node.end_byte]
+        snippet = code[node.start_byte : node.end_byte]
         snippet = snippet.replace("\n", "\\n")
 
         g.add_node(
@@ -61,42 +64,66 @@ def _tree_to_graph(tree: Tree, code_str: str) -> nx.DiGraph:
     return g
 
 
-def parse_code(code: str, language: SUPPORTED_LANGUAGES) -> nx.DiGraph:
+def _parse_code(code: str, language: SUPPORTED_LANGUAGES) -> Tree:
     parser = Parser(LANGUAGE_MAP[language])
-    tree = parser.parse(bytes(code, "utf8"))
-    return _tree_to_graph(tree, code)
+    return parser.parse(bytes(code, "utf8"))
 
 
-def parse_data_frame(data: pd.DataFrame) -> list[nx.DiGraph]:
+def create_graph(code: str, language: SUPPORTED_LANGUAGES) -> Data:
+    tree = _parse_code(code, language)
+    nodes, edges = [], []
+    info = {
+        "depth": [],
+        "num_children": [],
+        "type_id": [],
+        "is_named": [],
+        "length": [],
+    }
+
+    stack: list[tuple[Node, int | None, int]] = [(tree.root_node, None, 0)]
+
+    while stack:
+        node, parent_gid, depth = stack.pop(0)
+        gid = len(nodes)
+
+        nodes.append(node)
+        info["depth"].append(depth)
+        info["num_children"].append(len(node.children))
+        info["type_id"].append(node.kind_id)
+        info["is_named"].append(node.is_named)
+        info["length"].append(node.end_byte - node.start_byte)
+
+        if parent_gid is not None:
+            edges.append((parent_gid, gid))
+            edges.append((gid, parent_gid))
+
+        for child in node.children:
+            stack.append((child, gid, depth + 1))
+
+    return Data(
+        x=torch.stack(
+            [
+                torch.tensor(info["type_id"], dtype=torch.long),
+                torch.tensor(info["depth"], dtype=torch.long),
+                torch.tensor(info["num_children"], dtype=torch.long),
+                torch.tensor(info["is_named"], dtype=torch.long),
+                torch.tensor(info["length"], dtype=torch.long),
+            ],
+            dim=-1,
+        ),
+        edge_index=torch.tensor(edges, dtype=torch.long),
+    )
+
+
+def parse_data_frame(data: pd.DataFrame) -> list[Data]:
     parsed_codes = []
     for _, row in data.iterrows():
-        graph = parse_code(row["code"], row["language"])
-        parsed_codes.append(graph)
+        parsed_codes.append(create_graph(row["code"], row["language"]))
     return parsed_codes
 
 
 if __name__ == "__main__":
-    path = Path("SemEval-2026-Task13")
-    dir_paths = {name: path / f"task_{name}" for name in ["a", "b", "c"]}
-    datasets = {
-        name: {
-            "training": dir_paths[name] / f"task_{name}_training_set.parquet"
-            if name == "b"
-            else dir_paths[name] / f"task_{name}_training_set_1.parquet",
-            "validation": dir_paths[name] / f"task_{name}_validation_set.parquet",
-            "test": dir_paths[name] / f"task_{name}_test_set_sample.parquet",
-        }
-        for name in ["a", "b", "c"]
-    }
+    from .utils import load_data
 
-    data = pd.read_parquet(datasets["b"]["training"])
-    new_data = data.sample(5000)
-    new_data["word_count"] = new_data["code"].str.split().str.len()
-    new_data["code_len"] = [len(x) for x in parse_data_frame(new_data)]
-    del new_data["code"]
-    del new_data["generator"]
-    del new_data["label"]
-    new_data["ratio"] = new_data["word_count"] / new_data["code_len"]
-    new_table = new_data.groupby(["language"]).mean()
-    new_table["final_ratio"] = new_table["word_count"] / new_table["code_len"]
-    print(new_table)
+    new_data = load_data("b", "train").sample(1)
+    print(parse_data_frame(new_data)[0])
