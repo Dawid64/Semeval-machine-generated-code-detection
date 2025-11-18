ec0c08c350dc2ac576235dd8f718bd764e0eba99
Dawid64 <dawid.siera@gmail.com>
2025-11-18 15:53:33 +0100

    Change from click to google's fire

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,5 @@
 from typing import Literal
-import click
+import fire
 import torch
 from src.data_processing import download
 from src.data_processing.code_parser import SUPPORTED_LANGUAGES, create_graph
@@ -7,28 +7,20 @@ from src.train import Trainer
 from src.models import __all_models__, AVAILABLE_MODELS
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.add_command
-@click.command()
-@click.option("--dataset", "-d", default="a", help="Task name <'a', 'b', 'c'>")
-@click.option(
-    "--dataset_size",
-    default=None,
-    type=int,
-    help="Number of samples taken for training",
-)
-@click.option("--save", default="model.pth", help="Path to where to save the model")
-@click.argument("name")
 def train(
-    dataset: Literal["a", "b", "c"],
-    dataset_size: None | int,
-    save: str,
     name: AVAILABLE_MODELS,
+    save: str = "model.pth",
+    dataset: Literal["a", "b", "c"] = "a",
+    dataset_size: None | int = None,
 ):
+    """
+    Trains the model
+    Args:
+        name (AVAILABLE_MODELS): Name of the model
+        save (str, optional): where to save the model. Defaults to "model.pth".
+        dataset (Literal[&quot;a&quot;, &quot;b&quot;, &quot;c&quot;], optional): name of dataset. Defaults to "a".
+        dataset_size (None | int, optional): Number of samples that will be taken into dataset, if None it takes whole dataset. Defaults to None.
+    """
     model = __all_models__[name]
     trainer = Trainer(
         model, dataset_name=dataset, dataset_part=dataset_size, save_path=save
@@ -36,23 +28,23 @@ def train(
     trainer.train()
 
 
-@cli.add_command
-@click.command()
-@click.option(
-    "--from_file", "-f", default="model.pth", help="Path to where the model is saved"
-)
-@click.option("--language", "-l", default="Python", help="Uppercase code language name")
-@click.argument("name")
-@click.argument("path")
 def check(
-    from_file: str,
-    language: SUPPORTED_LANGUAGES,
     name: AVAILABLE_MODELS,
+    language: SUPPORTED_LANGUAGES,
     path: str,
+    model_path: str = "model.pth",
 ):
+    """Uses given from user model to perform interference on given file
+
+    Args:
+        name (AVAILABLE_MODELS): Name of model.
+        language (SUPPORTED_LANGUAGES): Language in uppercase for example Python.
+        path (str): path to the file
+        model_path (str, optional): Path to the model. Defaults to "model.pth".
+    """
     with open(path, "r") as file:
         code = "\n".join(file.readlines())
-    with open(from_file, "rb") as file:
+    with open(model_path, "rb") as file:
         state_dict = torch.load(file, "cpu")
     code_tree = create_graph(code, language)
     model = __all_models__[name]
@@ -61,7 +53,15 @@ def check(
     print(output)
 
 
-cli.add_command(click.command(download))
+def cli():
+    fire.Fire(
+        {
+            "download": download,
+            "train": train,
+            "check": check,
+        }
+    )
+
 
 if __name__ == "__main__":
     cli()
